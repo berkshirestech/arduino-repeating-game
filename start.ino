96158292c22ce5dace7b09151b4346ad6c2c5e58
Joseph Method <tristil@gmail.com>
2014-08-05 20:59:43 -0400

    WIP multiple LEDs

--- start.ino
+++ start.ino
@@ -1,9 +1,16 @@
 const int buttonPin = 2;     // the number of the pushbutton pin
-const int ledPin =  13;      // the number of the LED pin
+const int outputLedPin = 13;      // the number of the LED pin
+const int successLedPin = 12;
+const int failureLedPin = 11;
 const int ACTION_LENGTH = 17;
+
+enum ActionType {
+  FLASH, PAUSE
+};
+
 struct LedAction {
   long length;
-  int type; // 0 = pause 1 = flash
+  ActionType type;
 };
 
 LedAction ledActions[ACTION_LENGTH] = {};
@@ -16,7 +23,7 @@ void playback()
 
   for (int i = 0; i < numActions; i++)  {
     LedAction action = ledActions[i];
-    if(action.type == 1) { // pause
+    if(action.type == PAUSE) {
       delay(action.length);
     } else {
       flash(action.length);
@@ -28,19 +35,24 @@ int lastTime;
 
 void record()
 {
+  if(count == 0) {
+    success();
+    failure();
+    success();
+  }
   if(digitalRead(buttonPin) == LOW){
-    int currTime = millis();
     if(count > 0) {
-      LedAction pause = {currTime - lastTime, 0};  
+      int currTime = millis();
+      LedAction pause = {currTime - lastTime, PAUSE};  
       ledActions[count] = pause;
       count++; 
     }
 
-    lastTime = currTime;
     long holdStart = millis();
     while(digitalRead(buttonPin) == LOW);
-    
-    LedAction flash = {millis() - holdStart, 1};  
+    long holdEnd = millis();
+    lastTime = holdEnd;
+    LedAction flash = {holdEnd - holdStart, FLASH};  
     ledActions[count] = flash; 
     count++;
     
@@ -51,18 +63,30 @@ void record()
   }
 }
 
+void flashLed(int pin, long length) {
+ digitalWrite(pin, HIGH);
+ delay(length);
+ digitalWrite(pin, LOW);
+}
+
+void success() {
+  flashLed(successLedPin, 1000);
+}
+
+void failure() {
+  flashLed(failureLedPin, 1000);
+}
+
 void flash(long flashLength)
 {
- digitalWrite(ledPin, HIGH);
- delay(flashLength);
- digitalWrite(ledPin, LOW);
+  flashLed(outputLedPin, flashLength);
 }
 
 void setup() {
   // open the serial port at 9600 bps:
   Serial.begin(9600);
   // initialize the pins
-  pinMode(ledPin, OUTPUT);
+  pinMode(outputLedPin, OUTPUT);
   pinMode(buttonPin, INPUT);
 }
 
@@ -70,6 +94,9 @@ void loop() {
   if(isRecording){
     record();
   }else{
+    failure();
+    success();
+    failure();
     playback();
     isRecording = true;
   }
